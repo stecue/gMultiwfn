3fe480b97f5487354082578f08547dc6db85c67f
stecue <stecue@gmail.com>
2017-05-22 16:31:25 -0400

    Update the update script

--- update_src.sh
+++ update_src.sh
@@ -12,12 +12,21 @@ ln -s ../current_src.zip ./
 unzip current_src.zip
 cd Mult*
 find . -name '* *' -print0|xargs -0 rm -f
-rm GUI.f90 && rm *.a
+#rm file not mentioned in the noGUI Makefile
+rm GUI.f90 plot.f90 && rm *.a
 sed -i -e 's/grep/grep -a/g' noGUI.sh
 sh noGUI.sh && mv noGUI ../src_old
 popd && mv Multi*src_Linux src_full_old
+../gnuize.sh src_old
 pushd .
-#Get the new src
+#Get my patch for current src
+if [ -d src.orig ]; then
+    cp -pr ../src.orig src_gnu
+else
+    cp -pr ../src src_gnu
+fi
+diff -uNr src_old src_gnu > patch_gnu.diff
+#Get the new src and get the delta diff from the old to new ifort src
 ln -s ../latest_src.zip ./
 unzip latest_src.zip
 cd Mult*
@@ -26,21 +35,23 @@ rm GUI.f90 && rm *.a
 sed -i -e 's/grep/grep -a/g' noGUI.sh
 sh noGUI.sh && mv noGUI ../src_new
 popd && mv Multi*src_Linux src_full_new
-../gnuize.sh src_old
 ../gnuize.sh src_new
 diff -uNr src_old src_new > src_update.diff
 #backup the original src
-cd ..
-if [ -d src.orig ];then
-    rm -r src
-    cp -pr src.orig src
-else
-    cp -pr src src.orig
-fi
-#apply the patch
-cd src
-patch -p1 < ../$dirdiff/src_update.diff
-cd ..
+# cd ..
+# if [ -d src.orig ];then
+#     rm -r src
+#     cp -pr src.orig src
+# else
+#     cp -pr src src.orig
+# fi
+# #apply the patch
+# cd src
+# patch -p1 < ../$dirdiff/src_update.diff
+cd src_new patch -p1 < ../patch_gnu.diff
+cd .. #we are in $dirdiff
+rm -r ../src
+cp -r src_new ../src
 # rename .zip #should be down manually
 #mv current_src.zip previous_src.zip
 #mv latest_src.zip current_src.zip
